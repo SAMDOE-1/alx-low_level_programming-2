99aaebdfd3d5c779dc7d45d9ac6b9cafdcbdb760
SAMDOE-1 <samueldoe763@gmail.com>
2022-07-01 14:41:15 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
-                {
+{
 			putchar(ch);
 		}
 		ch++;
@@ -20,4 +20,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-	           		
